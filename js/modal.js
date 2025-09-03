3b3f0d4fe1188fdad8acb0a2fe08a016ce6cf4dc
Roman Zeno <zeno385@gmail.com>
2025-09-03 22:40:00 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -3,3 +3,19 @@ const menuBtn = document.querySelector(".header-icon-menu");
 menuBtn.addEventListener("click", () => {
   //   console.log("Привет Алисия, мы клыкнули на кнопку меню");
 });
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//     document.body.classList.toggle("no-scroll");
+//   }
+// })();
